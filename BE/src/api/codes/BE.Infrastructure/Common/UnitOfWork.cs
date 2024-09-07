9f419b7ff0c3197e3b511928d3129283505f1531
luctd <luctd@newwave.vn>
2024-09-07 12:17:24 +0700

    [luctd] update infrastructure

---
+++ BE/src/api/codes/BE.Infrastructure/Common/UnitOfWork.cs
@@ -0,0 +1,138 @@
+﻿using BE.Domain.Abstractions;
+using BE.Domain.Abstractions.IEntities;
+using BE.Domain.Abstractions.UnitOfWork;
+using BE.Domain.Entities.Users;
+using BE.Infrastructure.Repositories;
+using BE.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Data;
+using System.Security.Claims;
+
+namespace BE.Infrastructure.Common
+{
+    public class UnitOfWork : IUnitOfWork , IDisposable
+    {
+        private readonly ApplicationDbContext context;
+        private readonly ClaimsPrincipal claimsPrincipal;
+        private IDbContextTransaction? _currentTransaction;
+        public IDbContextTransaction? GetCurrentTransaction()
+        {
+            return _currentTransaction;
+        }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            this.context = context;          
+        }
+
+        // Interface Repository
+        public IUserRepository userRepository;
+        public IUserRepository UserRepository => userRepository = new UserRepository(context);     
+        public async Task BeginTransactionAsync()
+        {
+            if (_currentTransaction == null)
+            {
+                _currentTransaction = await context.Database.BeginTransactionAsync(IsolationLevel.ReadCommitted);
+            }
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            if (_currentTransaction == null) throw new ArgumentNullException(nameof(this._currentTransaction));
+
+            try
+            {
+                await SaveChangesAsync();
+                await _currentTransaction.CommitAsync();
+            }
+            catch
+            {
+                RollBack();
+                throw;
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void RollBack()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            this.BeforeSaveChanges();
+            var result = await context.SaveChangesAsync(cancellationToken);
+            //await mediator.DispatchDomainEvents(this);
+            return result;
+        }
+
+        private void BeforeSaveChanges()
+        {
+            foreach (var entry in context.ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        if (entry.Entity is EntityAuditBase added)
+                        {
+                            added.CreatedDate = DateTime.UtcNow;
+                        }
+
+                        if (entry.Entity is IUserTracking<Guid> hasTrace)
+                        {
+                            hasTrace.CreatedBy = new Guid(); //this.claimsPrincipal.GetUserId();
+                            hasTrace.CreatedByName = "Admin"; //this.claimsPrincipal.GetUserName();
+                        }
+
+                        break;
+                    case EntityState.Modified:
+                        if (entry.Entity is EntityAuditBase modified)
+                        {
+                            modified.LastModifiedDate = DateTime.UtcNow;
+                        }
+
+                        if (entry.Entity is IUserTracking<Guid> trace)
+                        {
+                            trace.ModifiedBy = new Guid(); //claimsPrincipal.GetUserId();
+                            trace.ModifiedByName = "Admin"; // claimsPrincipal.GetUserName();
+                        }
+
+                        break;
+                    case EntityState.Deleted:
+
+                        if (entry.Entity is ISoftDelete hasIsDeleted)
+                        {
+                            hasIsDeleted.IsDeleted = true;
+                            entry.State = EntityState.Modified;
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public async void Dispose() => await context.DisposeAsync();
+        
+    }
+}
---
+++ BE/src/api/codes/BE.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using BE.Domain.Entities.Users;
+using BE.Infrastructure.Abstractions;
+using BE.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE.Infrastructure.Repositories
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public void Delete(User entity)
+        {
+            context.Users.Remove(entity);
+        }
+
+        public async Task<User> FindByIdAsync(Guid id)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(u => u.Id == id);
+            return user;
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            var query = context.Users.AsQueryable();
+            return query;
+        }
+
+        public async void Insert(User entity)
+        {
+            await context.Users.AddAsync(entity);
+        }
+ 
+        public void Update(User entity)
+        {
+            context.Users.Update(entity);
+        }
+    }
+}
