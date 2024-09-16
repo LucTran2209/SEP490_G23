97e875a6dbcc71ee86da5bcd6e53ca8f380de677
luctd <luctd@newwave.vn>
2024-09-16 23:23:16 +0700

    [luctd] update entities

---
+++ BE/src/api/codes/BE.Domain/Entities/Categories/Category.cs
@@ -0,0 +1,14 @@
+﻿using BE.Domain.Abstractions;
+using BE.Domain.Abstractions.IEntities;
+using BE.Domain.Entities.Products;
+
+namespace BE.Domain.Entities.Categories
+{
+    public class Category : EntityAuditBase, IEntityBase<Guid>
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public virtual ICollection<Product>? Products { get; set; }
+    }
+}
