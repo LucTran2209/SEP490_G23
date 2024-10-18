9db35f69fa53f8931536c96accbf69c9d095f8fb
luctd2209 <luctd2209@gmail.com>
2024-10-18 18:04:20 +0700

    add smpt service

---
+++ BE/src/api/codes/BE.Application/DependencyInjections/SystemConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace BE.Application.DependencyInjections
+{
+    public class SystemConfig
+    {
+        public string? PlatformName { get; set; }
+        public string? BasePath { get; set; }
+        public string? ForgotPasswordUrl { get; set; }
+    }
+}
---
+++ BE/src/api/codes/BE.Application/Services/Mails/MailService.cs
@@ -0,0 +1,32 @@
+﻿using BE.Application.Abstractions.ServiceInterfaces;
+using BE.Application.DependencyInjections;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+
+namespace BE.Application.Services.Mails
+{
+    public class MailService : IMailService
+    {
+        private readonly SmtpConfig _smtpConfig;
+
+        public MailService(IOptions<SmtpConfig> smtpConfig)
+        {
+            _smtpConfig = smtpConfig.Value;
+        }
+
+        public async Task SendMailAsync(string? From, string To, string subject, string body)
+        {
+            SmtpClient emailClient = new SmtpClient(_smtpConfig.SmtpServer, _smtpConfig.Port);
+            emailClient.EnableSsl = true;
+            emailClient.Credentials = new NetworkCredential(_smtpConfig.Email, _smtpConfig.Password);
+
+            MailMessage message = new MailMessage($"{_smtpConfig.Email}", To);
+            message.Subject = subject;
+            message.IsBodyHtml = true;
+            message.Body = body;
+
+            await emailClient.SendMailAsync(message);
+        }
+    }
+}
