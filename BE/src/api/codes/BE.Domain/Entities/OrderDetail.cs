ecb16d51148d4b1b1e911babf627f99d3e2b1c35
minmetmoi <minh1182002@gmail.com>
2024-10-21 01:38:25 +0700

    fix database

---
+++ BE/src/api/codes/BE.Domain/Entities/OrderDetail.cs
@@ -0,0 +1,13 @@
+﻿using BE.Domain.Abstractions;
+
+namespace BE.Domain.Entities
+{
+    public class OrderDetail : EntityAuditBase
+    {
+        public Guid ProductId { get; set; }
+        public Guid OrderId { get; set; }
+        public int Quantity { get; set; }
+        public virtual Order Order { get; set; } = null!;
+        public virtual Product Product { get; set; } = null!;
+    }
+}
--- BE/src/api/codes/BE.Persistence/Common/ConstantTableNames.cs
+++ BE/src/api/codes/BE.Persistence/Common/ConstantTableNames.cs
@@ -6,10 +6,13 @@
         internal const string Roles = nameof(Roles);
         internal const string UserRoles = nameof(UserRoles);
         internal const string Rentals = nameof(Rentals);
-        internal const string RentalRequests = nameof(RentalRequests);
-        internal const string RentalRequestStatuses = nameof(RentalRequestStatuses);  
-        internal const string RentalRequestProducts = nameof(RentalRequestProducts);  
-        internal const string RentalShops = nameof(RentalShops);  
+        //internal const string RentalRequests = nameof(RentalRequests);
+        //internal const string RentalRequestStatuses = nameof(RentalRequestStatuses);  
+        internal const string Orders = nameof(Orders);
+        internal const string OrderDetails = nameof(OrderDetails);
+        internal const string OrderStatuses = nameof(OrderStatuses);
+        internal const string RentalRequestProducts = nameof(RentalRequestProducts);
+        internal const string RentalShops = nameof(RentalShops);
         internal const string ActivityLogs = nameof(ActivityLogs);
         internal const string Categories = nameof(Categories);
         internal const string FieldCategories = nameof(FieldCategories);
