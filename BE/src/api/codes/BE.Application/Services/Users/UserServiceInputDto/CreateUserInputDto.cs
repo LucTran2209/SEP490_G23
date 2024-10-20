85ac3abb11e9947f640ed7a7d1170c9266bbffe7
minmetmoi <minh1182002@gmail.com>
2024-10-20 20:30:02 +0700

    [minhnn] fix bug gender fasle and thêm nốt validate

--- BE/src/api/codes/BE.Application/Services/Users/UserServiceInputDto/CreateUserInputDto.cs
+++ BE/src/api/codes/BE.Application/Services/Users/UserServiceInputDto/CreateUserInputDto.cs
@@ -8,7 +8,7 @@
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
-        public bool Gender { get; set; }
+        public bool? Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string? Introduction { get; set; }
         public string? AvatarPersonal { get; set; }
--- BE/src/api/codes/BE.Application/Services/Users/UserServiceInputDto/UpadteUserInputDto.cs
+++ BE/src/api/codes/BE.Application/Services/Users/UserServiceInputDto/UpadteUserInputDto.cs
@@ -7,7 +7,7 @@
         public string Email { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
         public string Address { get; set; } = null!;
-        public bool Gender { get; set; }
+        public bool? Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
     }
 }
