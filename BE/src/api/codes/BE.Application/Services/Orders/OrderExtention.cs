5e117a43b6484ef2663994c59477d37886796bc8
minmetmoi <minh1182002@gmail.com>
2024-10-21 20:36:59 +0700

    api create order

---
+++ BE/src/api/codes/BE.Application/Services/Orders/OrderExtention.cs
@@ -0,0 +1,17 @@
+﻿using BE.Application.Services.Order.OrderServiceInputDto;
+using BE.Domain.Entities;
+
+public static class OrderExtention
+{
+    public static Order ToEntity(this CreateOrderInputDto inputDto)
+    {
+        var order = new Order();
+        order.Address = inputDto.Address;
+        order.UserId = inputDto.UserId;
+        order.StartDate = inputDto.StartDate;
+        order.EndDate = inputDto.EndDate;
+        order.Note = inputDto.Note;
+        return order;
+    }
+}
+
---
+++ BE/src/api/codes/BE.Application/Services/Orders/OrderServiceInputDto/CreateOrderInputDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BE.Application.Services.Order.OrderServiceInputDto
+{
+    public class CreateOrderInputDto
+    {
+        public Guid UserId { get; set; }
+        public string? Address { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? Note { get; set; }
+    }
+}
