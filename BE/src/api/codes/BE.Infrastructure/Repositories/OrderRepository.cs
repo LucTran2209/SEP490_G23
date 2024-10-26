cad7c339b63773a47ee5fe43aac16870edb30432
minmetmoi <minh1182002@gmail.com>
2024-10-26 18:57:59 +0700

    [minhnn] fix name shop and product

--- BE/src/api/codes/BE.Infrastructure/Repositories/OrderRepository.cs
+++ BE/src/api/codes/BE.Infrastructure/Repositories/OrderRepository.cs
@@ -30,10 +30,10 @@ namespace BE.Infrastructure.Repositories
 
         public IQueryable<Order> GetAll()
         {
-            var query = context.Orders.Include(o => o.OrderDetails).ThenInclude(o => o.Product).ThenInclude(o => o.RentalShop)
-                                      .Include(o => o.OrderDetails).ThenInclude(o => o.Product).ThenInclude(o => o.ProductImages)
-                                      .Include(o => o.User)
+            var query = context.Orders.Include(o => o.User)
                                       .Include(o => o.OrderStatuses)
+                                      .Include(o => o.OrderDetails).ThenInclude(o => o.Product).ThenInclude(o => o.RentalShop)
+                                      .Include(o => o.OrderDetails).ThenInclude(o => o.Product).ThenInclude(o => o.ProductImages)
                                       .AsQueryable();
             return query;
         }
