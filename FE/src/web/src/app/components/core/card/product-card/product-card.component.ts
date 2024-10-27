9a0f2ed469eddd96eb802bfdf09d570a6f2a3c96
Nam Huu Ngo <121742978+devnamnh2k2@users.noreply.github.com>
2024-10-27 13:05:02 +0700

    update fix bug

--- FE/src/web/src/app/components/core/card/product-card/product-card.component.ts
+++ FE/src/web/src/app/components/core/card/product-card/product-card.component.ts
@@ -44,8 +44,7 @@ export class ProductCardComponent implements OnInit {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-
-    
+    this.handleAssginInfo();
   }
 
   
--- FE/src/web/src/app/features/lessor/lessor.module.ts
+++ FE/src/web/src/app/features/lessor/lessor.module.ts
@@ -5,9 +5,11 @@ import { LessorRoutingModule } from './lessor-routing.module';
 import { SharedModule } from '../../components/shared/shared.module';
 import { ManagePostComponent } from './components/manage-post/manage-post.component';
 import { ManagerShopComponent } from './components/manager-shop/manager-shop.component';
+import { ManageOrderComponent } from './components/manage-order/manage-order.component';
+import { OrderDetailComponent } from './components/manage-order/order-detail/order-detail.component';
 
 @NgModule({
-  declarations: [ManagePostComponent, ManagerShopComponent],
+  declarations: [ManagePostComponent, ManagerShopComponent, ManageOrderComponent, OrderDetailComponent],
   imports: [CommonModule, LessorRoutingModule, SharedModule],
 })
 export class LessorModule {}
