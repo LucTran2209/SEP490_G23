5854910e9583ab22aad4c30a29543421ac2846d7
Nam Huu Ngo <121742978+devnamnh2k2@users.noreply.github.com>
2024-11-16 21:49:44 +0700

    update fix reigster auth

--- FE/src/web/src/app/features/auth/auth-routing.module.ts
+++ FE/src/web/src/app/features/auth/auth-routing.module.ts
@@ -11,6 +11,7 @@ import { VerifyEmailComponent } from './components/verify-email/verify-email.com
 import { checkRegisterCommonGuard } from '../../guards/check-register-common.guard';
 import { WarningOutRegisterGuard } from '../../guards/warning-out-register.guard';
 
+
 const routes: Routes = [
   {
     path: '',
