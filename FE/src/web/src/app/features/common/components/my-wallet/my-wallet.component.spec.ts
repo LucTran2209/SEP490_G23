4142a23e9b680a4eff0d9d4e8800da2cab995786
Nam Huu Ngo <121742978+devnamnh2k2@users.noreply.github.com>
2024-11-19 19:19:23 +0700

    update UI dashboard

--- FE/src/web/src/app/features/common/components/my-wallet/my-wallet.component.spec.ts
+++ FE/src/web/src/app/features/common/components/my-wallet/my-wallet.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+<<<<<<<< HEAD:FE/src/web/src/app/features/common/components/my-wallet/my-wallet.component.spec.ts
 import { MyWalletComponent } from './my-wallet.component';
 
 describe('MyWalletComponent', () => {
@@ -13,6 +14,21 @@ describe('MyWalletComponent', () => {
     .compileComponents();
     
     fixture = TestBed.createComponent(MyWalletComponent);
+========
+import { OrderLatestComponent } from './order-latest.component';
+
+describe('OrderLatestComponent', () => {
+  let component: OrderLatestComponent;
+  let fixture: ComponentFixture<OrderLatestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OrderLatestComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(OrderLatestComponent);
+>>>>>>>> 33e8bd0 (update UI dashboard):FE/src/web/src/app/features/lessor/components/overview/order-latest/order-latest.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- FE/src/web/src/app/features/lessor/components/overview/order-latest/order-latest.component.spec.ts
+++ FE/src/web/src/app/features/lessor/components/overview/order-latest/order-latest.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+<<<<<<<< HEAD:FE/src/web/src/app/features/lessor/components/overview/order-latest/order-latest.component.spec.ts
 import { OrderLatestComponent } from './order-latest.component';
 
 describe('OrderLatestComponent', () => {
@@ -13,6 +14,21 @@ describe('OrderLatestComponent', () => {
     .compileComponents();
     
     fixture = TestBed.createComponent(OrderLatestComponent);
+========
+import { OrderStatisticComponent } from './order-statistic.component';
+
+describe('OrderStatisticComponent', () => {
+  let component: OrderStatisticComponent;
+  let fixture: ComponentFixture<OrderStatisticComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OrderStatisticComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(OrderStatisticComponent);
+>>>>>>>> 33e8bd0 (update UI dashboard):FE/src/web/src/app/features/lessor/components/overview/statistics/order-statistic/order-statistic.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
