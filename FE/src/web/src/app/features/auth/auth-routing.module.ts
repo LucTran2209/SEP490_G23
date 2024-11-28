7c95cfd6fd075f13c72b97695e6e581a893a9eb4
Nam Huu Ngo <121742978+devnamnh2k2@users.noreply.github.com>
2024-11-28 14:51:35 +0700

    fix bug 16,7

--- FE/src/web/src/app/features/auth/auth-routing.module.ts
+++ FE/src/web/src/app/features/auth/auth-routing.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthLayoutComponent } from '../../components/layout/auth-layout/auth-layout.component';
 import { AuthSlug } from '../../configs/api.configs';
+import { WarningOutRegisterGuard } from '../../guards/warning-out-register.guard';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
-import { checkRegisterCommonGuard } from '../../guards/check-register-common.guard';
-import { WarningOutRegisterGuard } from '../../guards/warning-out-register.guard';
-
 
 const routes: Routes = [
   {
@@ -31,6 +28,7 @@ const routes: Routes = [
         path: AuthSlug.ForgotPassWord.label,
         component: ForgotPasswordComponent,
         title: AuthSlug.ForgotPassWord.title,
+        canDeactivate: [WarningOutRegisterGuard],
       },
       {
         path: AuthSlug.ResetPassWord.label,
@@ -41,7 +39,7 @@ const routes: Routes = [
         path: AuthSlug.Register.label,
         component: RegisterComponent,
         title: AuthSlug.Register.title,
-        canDeactivate: [WarningOutRegisterGuard]
+        canDeactivate: [WarningOutRegisterGuard],
       },
     ],
   },
