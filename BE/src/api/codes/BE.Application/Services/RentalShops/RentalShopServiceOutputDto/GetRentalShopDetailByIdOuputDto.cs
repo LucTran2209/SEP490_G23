e3ddbccaa55a468ddffe0bcad4cded915ebb1f3f
luctd <luctd2209@gmail.com>
2024-11-29 22:53:04 +0700

    commit

--- BE/src/api/codes/BE.Application/Services/RentalShops/RentalShopServiceOutputDto/GetRentalShopDetailByIdOuputDto.cs
+++ BE/src/api/codes/BE.Application/Services/RentalShops/RentalShopServiceOutputDto/GetRentalShopDetailByIdOuputDto.cs
@@ -5,7 +5,7 @@ namespace BE.Application.Services.RentalShops.RentalShopServiceOutputDto
     public class GetRentalShopDetailByIdOuputDto : RentalShopDto
     {
         public DateTime CreateDate { get; set; }
-
+        public int NumberOfProduct {  get; set; }
         public int NumberOfRenter {  get; set; }
         public int NumberOfVote { get; set; }
         public decimal AvegateVote {  get; set; }
