b2f589d308a20bd23b71b9b56a4d4fc567073b22
Nam Huu Ngo <121742978+devnamnh2k2@users.noreply.github.com>
2024-12-07 18:01:57 +0700

    update link

---
+++ FE/src/web/src/app/components/base-components/base-components.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BaseComponentsComponent } from './base-components.component';
+
+describe('BaseComponentsComponent', () => {
+  let component: BaseComponentsComponent;
+  let fixture: ComponentFixture<BaseComponentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BaseComponentsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BaseComponentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- FE/src/web/src/app/features/common/components/home-page/components/brief-procedure-rental/brief-procedure-rental.component.ts
+++ FE/src/web/src/app/features/common/components/home-page/components/brief-procedure-rental/brief-procedure-rental.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { contentCardProcessRental_renter, IItemProcessRental } from '../../../../static/howitorder/howitorder.component';
+import {
+  contentCardProcessRental_renter,
+  IItemProcessRental,
+} from '../../../../static/howitorder/howitorder.component';
+import { BaseComponentsComponent } from '../../../../../../components/base-components/base-components.component';
 
 @Component({
   selector: 'app-brief-procedure-rental',
   templateUrl: './brief-procedure-rental.component.html',
-  styleUrl: './brief-procedure-rental.component.scss'
+  styleUrl: './brief-procedure-rental.component.scss',
 })
-export class BriefProcedureRentalComponent {
+export class BriefProcedureRentalComponent extends BaseComponentsComponent {
   commonProcessRental_renter: IItemProcessRental[] =
     contentCardProcessRental_renter;
 }
